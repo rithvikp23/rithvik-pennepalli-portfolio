5a6630941559c61ef21bc9a177adfaab2a1ea8b9
rithvik pennepalli <rithvikpennepalli@rithviks-MacBook-Pro.local>
2026-08-31 17:25:23 -0400

    Fix Vite dev dependency scan

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  optimizeDeps: {
+    entries: ['index.html'],
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./', import.meta.url)),
